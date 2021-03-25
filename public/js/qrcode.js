8b08f9b1b3aee79cf65a8edf6a8663456d69795e
egboyguda <e.gboyguda@gmail.com>
2021-03-25 09:32:45 +0800

    added alert message

--- public/js/qrcode.js
+++ public/js/qrcode.js
@@ -8,12 +8,11 @@ function onScanSuccess(qrCodeMessage) {
             axios.get(`/scan/${qrCodeMessage}`)
                 .then(function(res){
                     //p.innerHTML = res.data;
-                    console.log(res.data)
-                    document.getElementById('p').textContent+=`Succesfuly Scan`
+                    alert('Successfully Scan')
+                    //document.getElementById('p').textContent+=`Succesfuly Scan`
                 })
-
-
             html5QrcodeScanner.clear();
+            window.location ='/qrcode'
         }
 
     var html5QrcodeScanner = new Html5QrcodeScanner(
