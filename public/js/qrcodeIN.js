749ee75256395294f99cc2e316130f4e0a3fcb11
egboyguda <e.gboyguda@gmail.com>
2021-03-29 13:59:07 +0800

    TRY TO FIX MANY QRSEND TO DATABASE

--- public/js/qrcodeIN.js
+++ public/js/qrcodeIN.js
@@ -1,7 +1,7 @@
 function noop() {}
 async function onScanSuccess(qrCodeMessage) {
   // handle on success condition with the decoded message
-  onScanSuccess = noop;
+  onScanSuccess = await noop;
   axios.post(`/qrcode/scan/${qrCodeMessage}/in`).then(function (res) {
     //p.innerHTML = res.data;
     alert(`Successfully Scan ${res.data.toUpperCase()}`);
