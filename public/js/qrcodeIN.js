709cea2c43280d4e9e2b10bb73cea4b124085b89
egboyguda <e.gboyguda@gmail.com>
2021-03-29 14:16:01 +0800

    add a flag to detect if executed

--- public/js/qrcodeIN.js
+++ public/js/qrcodeIN.js
@@ -1,20 +1,17 @@
-function noop() {}
+function noop() {
+  return (window.location = 'qrcode/in');
+}
 async function onScanSuccess(qrCodeMessage) {
   // handle on success condition with the decoded message
-  html5QrcodeScanner.clear();
-  executed = false;
-  return function () {
-    if (!executed) {
-      executed = true;
-      axios.post(`/qrcode/scan/${qrCodeMessage}/in`).then(function (res) {
-        //p.innerHTML = res.data;
-        alert(`Successfully Scan ${res.data.toUpperCase()}`);
-
-        window.location = '/qrcode/in';
-      });
-    }
-  };
+  onScanSuccess = await noop;
+  axios.post(`/qrcode/scan/${qrCodeMessage}/in`).then(function (res) {
+    //p.innerHTML = res.data;
+    alert(`Successfully Scan ${res.data.toUpperCase()}`);
+    window.location = '/qrcode/in';
+    html5QrcodeScanner.clear();
+  });
 }
+
 var html5QrcodeScanner = new Html5QrcodeScanner('reader', {
   fps: 10,
   qrbox: 250,
