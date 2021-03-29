da2b1ab88ec20cea233a90651ac96ae23a145ebd
egboyguda <e.gboyguda@gmail.com>
2021-03-29 13:54:22 +0800

    TRY TO FIX MANY QRSEND TO DATABASE

--- public/js/qrcodeIN.js
+++ public/js/qrcodeIN.js
@@ -2,16 +2,12 @@ function noop() {}
 async function onScanSuccess(qrCodeMessage) {
   // handle on success condition with the decoded message
   onScanSuccess = noop;
-  axios
-    .post(`/qrcode/scan/${qrCodeMessage}/in`)
-    .then(function (res) {
-      //p.innerHTML = res.data;
-      html5QrcodeScanner.clear();
-    })
-    .then(function () {
-      alert(`Successfully Scan ${res.data.toUpperCase()}`);
-      window.location = '/qrcode/in';
-    });
+  axios.post(`/qrcode/scan/${qrCodeMessage}/in`).then(function (res) {
+    //p.innerHTML = res.data;
+    alert(`Successfully Scan ${res.data.toUpperCase()}`);
+    window.location = '/qrcode/in';
+    html5QrcodeScanner.clear();
+  });
 }
 
 var html5QrcodeScanner = new Html5QrcodeScanner('reader', {
